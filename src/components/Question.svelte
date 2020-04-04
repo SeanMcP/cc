0b22eff7f4cb7dcf166f48cdda6c929d2d8bd272
Sean McPherson <git@seanmcp.com>
2020-04-04 14:45:59 -0400

    Add Quuestion component

---
+++ src/components/Question.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { getNumberedQuestion } from '../utils/get-numbered-question'
+    export let withoutTitle = false
+    export let data = {}
+
+    $: showTitle = !withoutTitle
+    $: numberedQuestion = getNumberedQuestion(data)
+</script>
+
+<p>{#if showTitle}<em>{numberedQuestion}</em> {/if}{data.answer}</p>
